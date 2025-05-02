b3b597a0492af87c547bf59c20e7b8b4ae27f6be
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2025-05-02 08:49:01 +0700

    menambahkan program bubblesort

--- Binarysearch.cpp
+++ Binarysearch.cpp
@@ -5,6 +5,58 @@ int element[10];
 int npanjang;
 int x;
 
+void input() { //prosedur input
+    while (true){
+        cout << "Masukkan banyaknya elemen pada array: ";
+        cin >> npanjang;
+        if (npanjang <= 20){
+            break;
+        }
+        else {
+            cout << "\nAraay maksimal 20 elemen.\n";
+        }
+    }
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Masukkan elemen array" << endl;
+    cout << "=====================" << endl;
+
+    for(int i=0;i<npanjang;i++){
+        cout << "Data ke-" << (i+1) << " = ";
+        cin >> element[i];
+    }
+}
+
+void bubbleSortArray(){
+    int pass = 1; //step 1
+   do{
+    for (int j=0;j<=npanjang-1-pass;j++){ //step 2
+        if(element[j]>element[j+1]){//step 3
+            int temp;
+            temp = element[j];
+            element[j] = element[j+1];
+            element[j+1] = temp;
+        }
+    }
+    pass = pass +1; //step 4
+   }while(pass <= npanjang-1); //step 5
+    
+}
+
+void display(){
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    cout << endl;
+    for (int j=0;j<npanjang;j++){
+        cout << element[j];
+        if (j<npanjang-1) {
+            cout << " --> ";
+        }
+    }
+}
+
 void binarysearch(){
     cout <<"masukkan element yang ingin dicari = ";
     cin >> x;
@@ -29,6 +81,5 @@ void binarysearch(){
 }
 
 int main(){
-
 
 }
